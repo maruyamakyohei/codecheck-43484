aeaa32a60fc387bac03656e8c0e9d44c047a9533
tie303310 <tie303310@S1504C10034-T1.tisnt.tis.co.jp>
2018-06-19 02:42:13 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,8 +3,47 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+
+			try {
+				// 引数をint型に変換
+				int param = Integer.parseInt(args[i]);
+
+				if (param < 0 || param > 1000) {
+					// 負整数または1000より大きい非負整数の場合は「invalid」を出力
+					System.out.println("invalid");
+				} else {
+
+					// 引数が3で割り切れるか
+					int mod3 = param % 3;
+					boolean idiot = false;
+					if (mod3 == 0) {
+						idiot = true;
+					}
+					// 引数に3が含まれるか
+					boolean stupid = false;
+					if (args[i].contains("3")) {
+						stupid = true;
+					}
+
+					if (idiot && stupid) {
+						// 3の倍数でかつ3のつく1000以下の非負整数
+						System.out.println("dump");
+					} else if(idiot) {
+						// 3の倍数の1000以下の非負整数
+						System.out.println("idiot");
+					} else if(stupid) {
+						// 3のつく1000以下の非負整数の場合
+						System.out.println("stupid");
+					} else {
+						// それ以外の1000以下の非負整数
+						System.out.println("smart");
+					}
+				}
+			} catch(NumberFormatException e) {
+				// NumberFormatException発生時は「invalid」を出力
+				System.out.println("invalid");
+			}
+
 		}
 	}
 }
